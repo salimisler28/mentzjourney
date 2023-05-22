2488326696fec956c3c4698abe6001270cb2c046
salimisler <dresizre@gmail.com>
2023-05-22 13:56:11 +0200

    focus handled

--- app/src/main/java/com/mentz/mentzjourney/presentation/main/MainActivity.kt
+++ app/src/main/java/com/mentz/mentzjourney/presentation/main/MainActivity.kt
@@ -12,21 +12,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mentz.mentzjourney.presentation.ui.theme.MentzjourneyTheme
 import dagger.hilt.android.AndroidEntryPoint
 import com.mentz.mentzjourney.R
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -38,12 +48,24 @@ class MainActivity : ComponentActivity() {
             val screenState = mainViewModel.screenState.collectAsState()
             val searchKey = mainViewModel.searchKey.collectAsState()
 
+            val focusManager = LocalFocusManager.current
+            val focusRequester = remember { FocusRequester() }
+
+            LaunchedEffect(key1 = Unit, block = {
+                delay(200)
+                focusRequester.requestFocus()
+            })
+
             MentzjourneyTheme {
                 SearchAndResult(
                     screenState = screenState.value,
+                    searchTextFieldFocusRequester = focusRequester,
                     searchKeyValue = searchKey.value,
                     onSearchKeyValueChange = {
                         mainViewModel.onSearchKeyChange(it)
+                    },
+                    onSearchClicked = {
+                        focusManager.clearFocus()
                     }
                 )
             }
@@ -54,15 +76,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SearchAndResult(
     screenState: ScreenState,
+    searchTextFieldFocusRequester: FocusRequester,
     searchKeyValue: String,
-    onSearchKeyValueChange: (String) -> Unit
+    onSearchKeyValueChange: (String) -> Unit,
+    onSearchClicked: () -> Unit,
 ) {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
         SearchBar(
+            focusRequester = searchTextFieldFocusRequester,
             value = searchKeyValue,
-            onValueChange = onSearchKeyValueChange
+            onValueChange = onSearchKeyValueChange,
+            onSearchClicked = onSearchClicked
         )
 
         when (screenState) {
@@ -104,8 +130,10 @@ fun EmptySearch() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
+    focusRequester: FocusRequester,
     value: String,
-    onValueChange: (String) -> Unit
+    onValueChange: (String) -> Unit,
+    onSearchClicked: () -> Unit
 ) {
     TextField(
         value = value,
@@ -116,6 +144,18 @@ fun SearchBar(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
+            .focusRequester(focusRequester),
+        singleLine = true,
+        maxLines = 1,
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onSearchClicked.invoke()
+            }
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Search
+        )
     )
 }
 
@@ -125,6 +165,8 @@ private fun PreviewSearchAndResult() {
     SearchAndResult(
         screenState = ScreenState.EmptySearch,
         searchKeyValue = "",
-        onSearchKeyValueChange = {}
+        onSearchKeyValueChange = {},
+        onSearchClicked = {},
+        searchTextFieldFocusRequester = FocusRequester()
     )
 }
